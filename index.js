55eaa4692946de019d177266c874c18080f35025
Hugo-22 <hugo.denis22@gmail.com>
2020-11-08 16:05:23 +0100

    excercise 2

--- index.js
+++ index.js
@@ -25,4 +25,33 @@ const getYoda = async () => {
         console.log(err)
     }
 } 
-getYoda()
+// getYoda()
+
+// Exercice 2
+// Créez une fonction qui retourne le nom de chaque espèce 
+// qui contient au moins 2 personnages
+const getSpecies = async () => {
+    try {
+        let page = 1
+        let lastPage = false
+        const species = []
+
+        while (!lastPage) {
+            let res = await axios.get('http://swapi.dev/api/species/', {
+                params: {
+                    page
+                }
+            })
+            for (const props in res.data.results) {
+                if (res.data.results[props].people.length >= 2) {
+                    species.push(res.data.results[props].name)
+                }
+            }
+            res.data.next ? page++ : lastPage = true
+        }
+        console.log(species)
+    } catch (err) {
+        console.log(err)
+    }
+}
+getSpecies()
