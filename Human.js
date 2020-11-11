51786a256dc8fb2a0583dfd38925253618617f7a
Hugo-22 <hugo.denis22@gmail.com>
2020-11-11 13:13:16 +0100

    class Human

---
+++ Human.js
@@ -0,0 +1,11 @@
+class Human {
+
+    constructor(name, height, mass, homeworld, films) {
+        this.name = name
+        this.height = height
+        this.mass = mass
+        this.homeworld = homeworld
+        this.films = films
+    }
+}
+module.exports = Human
